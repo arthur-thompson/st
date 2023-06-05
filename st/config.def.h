f90eee5e3a4b0da73c3166e56682224785bb243b
Arthur Thompson <102389665+arthur-thompson@users.noreply.github.com>
2023-06-05 09:25:38 +0000

    Update config.def.h

--- st/config.def.h
+++ st/config.def.h
@@ -184,8 +184,8 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release altscreen */
-	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},      0,      -1 },
-	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1},      0,      -1 },
+	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 7},      0,      -1 },
+	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 7},      0,      -1 },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
